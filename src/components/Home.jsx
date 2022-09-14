4cb182f02d78b5edac75139a92b07be58ec3634b
ingole8888 <ingole8888@gmail.com>
2022-09-14 10:29:17 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,9 @@ const Home = () => {
        
        <div className='max-w-screen-lg mx-auto flex flex-col items-center 
        justify-center h-full px-4 md:flex-row '>
-          <div className='flex flex-col justify -center h-full h-[18rem] sm h-[10rem]
+          <div className='flex flex-col justify -center h-full 
            '>
+            {/* h-[18rem] sm h-[10rem] */}
            <h2 className=' text-4xl sm:text-7xl font bold text-white
            '>I'm a Full Stack Developer</h2>
            <p className='text-gray-500 py-4  '>Enthusiastic Full Stack  Developer. Work on various projects in a Masai School
