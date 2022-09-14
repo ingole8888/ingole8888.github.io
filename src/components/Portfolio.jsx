8b56694c9199c5fcb7e4cc2b7873fe538fa11449
ingole8888 <ingole8888@gmail.com>
2022-09-14 13:56:02 +0530

    fdgfhfh

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -38,9 +38,9 @@ const Portfolio = () => {
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full md:h-screen text-white"
+      className="bg-gradient-to-b from-black to-gray-800 w-full md:h-full text-white "
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full h-[60rem]">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Projects
