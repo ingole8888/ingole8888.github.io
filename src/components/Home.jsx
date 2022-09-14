cb1c1e0c640549969fe8e1eeedb71f0bb62fd061
ingole8888 <ingole8888@gmail.com>
2022-09-14 10:22:54 +0530

    home about

---
+++ src/components/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { HiArrowNarrowRight } from "react-icons/hi";
+import HeroImage from "../assets/heroimage.jpg";
+
+const Home = () => {
+  return (
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black 
+    via-black to-gray-800 '>
+       
+       <div className='max-w-screen-lg mx-auto flex flex-col items-center 
+       justify-center h-full px-4 md:flex-row '>
+          <div className='flex flex-col justify -center h-full h-[18rem] sm h-[10rem]
+           '>
+           <h2 className=' text-4xl sm:text-7xl font bold text-white
+           '>I'm a Full Stack Developer</h2>
+           <p className='text-gray-500 py-4  '>Enthusiastic Full Stack  Developer. Work on various projects in a Masai School
+           during last week of a every unit using React-JS, Bootstrap, Chakra-UI and   
+             CSS for frontend and Node.js, Express and MongoDB for backend. Interested for a position
+           Software Developer to enhance my skills and to enhance the user experience.</p>
+           <div>
+                <button className=' group text-white w-fit px-6 py-3 my-2
+                flex items-center rounded-md bg-gradient-to-r
+                from-cyan-500 to-blue-500 cursor-pointer
+                '>Resume
+                   <span className='group-hover:rotate-90 duration-300'>
+                    <HiArrowNarrowRight size={25}/>
+                    </span>
+                </button>
+           </div>    
+          </div>
+           <div>
+            <img
+            src={HeroImage} 
+            alt="my personal profile"
+            className='rounded-2xl mx w-2/3 md:w-2/2'
+            />
+           </div>
+
+        </div>
+      
+
+    </div>
+  )
+}
+
+export default Home
