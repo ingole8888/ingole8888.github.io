4075f581b53ccc91861f823a9ece34873e4f7c52
ingole8888 <ingole8888@gmail.com>
2022-09-19 23:33:57 +0530

    github cal

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -42,6 +42,7 @@ const Contact = () => {
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Let's talk
             </button>
+            <div><h1>Email:ingole8888@gmail.com </h1><h1>Mob.: +91 9145638836</h1></div>
           </form>
         </div>
       </div>
