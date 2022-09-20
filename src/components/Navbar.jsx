15db8c8897fc339402a3160173a5c249ceef020a
ingole8888 <ingole8888@gmail.com>
2022-09-20 13:04:44 +0530

    gfhghffg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
              absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black
              to-gray-800 text-gray-500'>
                {links.map(({id,link}) =>( <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl ">
-                      <Link to={link} smooth duration={500}> {link}  </Link>
+                      <Link to={link} onClick={()=>setNav(!nav)} smooth duration={500}> {link}  </Link>
                  </li>
                  ))}
              </ul>
